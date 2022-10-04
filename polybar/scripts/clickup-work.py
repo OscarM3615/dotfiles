a18df2b6e16000e19e9b2218359a29ff78a0edc4
Óscar Miranda <oscarmiranda3615@gmail.com>
2022-10-04 13:59:13 -0600

    add new tasks counter

---
+++ polybar/scripts/clickup-work.py
@@ -0,0 +1,20 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+envfile = os.path.join(os.path.expanduser('~'), '.config', 'polybar', '.env')
+load_dotenv(envfile)
+
+API_KEY = os.environ.get('CLICKUP_API_KEY', '')
+
+response = requests.get(
+    'https://api.clickup.com/api/v2/folder/114538688', headers={'Authorization': API_KEY})
+
+if not response.ok:
+    exit()
+
+data = response.json()
+task_count = int(data['task_count'])
+
+if task_count > 0:
+    print(task_count)
