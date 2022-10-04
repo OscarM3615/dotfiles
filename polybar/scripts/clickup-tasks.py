91e4a6f89e5526e43a051ee34ba7a15422ca27ab
Óscar Miranda <oscarmiranda3615@gmail.com>
2022-10-04 13:04:46 -0600

    add clickup counter to polybar

---
+++ polybar/scripts/clickup-tasks.py
@@ -0,0 +1,23 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+envfile = os.path.join(os.path.expanduser('~'), '.config', 'polybar', '.env')
+load_dotenv(envfile)
+
+API_KEY = os.environ.get('CLICKUP_API_KEY', '')
+
+response = requests.get(
+    'https://api.clickup.com/api/v2/space/14976905/list', headers={'Authorization': API_KEY})
+
+if not response.ok:
+    exit()
+
+data = response.json()
+
+task_count = 0
+for lst in data['lists']:
+    task_count += lst.get('task_count', 0)
+
+if task_count > 0:
+    print(task_count)
